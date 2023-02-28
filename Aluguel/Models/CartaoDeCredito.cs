e42e3083aabd2180cda2fdc5fbfb98eb336b496d
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-02-27 23:46:13 -0300

    refactor: Remocao de DataAnnotations

--- Aluguel/Models/CartaoDeCredito.cs
+++ Aluguel/Models/CartaoDeCredito.cs
@@ -1,42 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Aluguel.Models
+﻿namespace Aluguel.Models
 {
     public class CartaoDeCredito
     {
-        [Key]
-        [Required]
         public int Id { get; set; }
-
-        [Required]
         public string Nome { get; set; }
-
-        [Required]
         public string Numero { get; set; }
-
-        [Required]
         public int MesValidade { get; set; }
-
-        [Required]
         public int AnoValidade { get; set; }
-
-        [Required]
         public int CodigoSeguranca { get; set; }
-
-        [Required]
         public EStatusCartao Status { get; set; }
-
-        //[Required]
-        //[ForeignKey()]
-        //public int IdCiclista { get; set; }
-
-        //[Required]
-        //[ForeignKey]
+        public int IdCiclista { get; set; }
         //public IList<Emprestimo> Emprestimos { get; set; }
-
-        [Required]
-        [ForeignKey("Devolucao")]
         public IList<Devolucao> Devolucoes { get; set; }
     }
 }
--- Aluguel/Models/Devolucao.cs
+++ Aluguel/Models/Devolucao.cs
@@ -1,35 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Aluguel.Models
+﻿namespace Aluguel.Models
 {
     public class Devolucao
     {
-
-        [Key]
-        [Required]
         public int Id { get; set; }
-
-        [Required]
         public DateTime DataHora { get; set; }
-
-        [Required]
         public float Valor { get; set; }
-
-        [Required]
         public DateTime DataHoraPagamento { get; set; }
-
-        //[Required]
-        //[ForeignKey()]
-        //public int EmprestimoId { get; set; }
-
-        //[Required]
-        //[ForeignKey()]
-        //public int TrancaId { get; set; }
-
-        [Required]
-        [ForeignKey("CartaoDeCredito")]
+        public int EmprestimoId { get; set; }
+        public int TrancaId { get; set; }
         public int CartaoCreditoId { get; set; }
-
     }
 }
