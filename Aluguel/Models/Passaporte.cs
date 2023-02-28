a616aaabfe4d7ae7919cf8e6ca21f7c02266c38f
Matheus Varella <varellamat@gmail.com>
2023-02-28 14:35:37 -0300

    feat: implementado a classe passaporte

---
+++ Aluguel/Models/Passaporte.cs
@@ -0,0 +1,10 @@
+﻿namespace Aluguel.Models
+{
+    public class Passaporte
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public DateTime DataValidade { get; set; }
+        public int PaisId { get; set; }
+    }
+}
