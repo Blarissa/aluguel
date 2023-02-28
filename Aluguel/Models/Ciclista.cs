dc47625be2e3d73c6917c506a9fca4c124b524c6
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-02-27 21:11:53 -0300

    feat:  implementando classe Ciclista

---
+++ Aluguel/Models/Ciclista.cs
@@ -0,0 +1,18 @@
+﻿namespace Aluguel.Models
+{
+    public class Ciclista
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string UrlFotoDocumento { get; set; }
+        public EStatusCiclista Status { get; set; }
+        public ENacionalidade Nacionalidade { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+        public DateTime DataHoraConfirmacao { get; set; }
+        public IList<CartaoCredito> Cartoes { get; set; }
+        public IList<Emprestimo> Emprestimos { get; set; }
+    }
+}
