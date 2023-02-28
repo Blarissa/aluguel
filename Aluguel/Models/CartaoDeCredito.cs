8ea084bcf405983423ad59d558c293e9bed1d952
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-02-27 22:24:10 -0300

    feat: Adicionado modelo CartaoDeCredito

---
+++ Aluguel/Models/CartaoDeCredito.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aluguel.Models
+{
+    public class CartaoDeCredito
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Numero { get; set; }
+
+        [Required]
+        public int MesValidade { get; set; }
+
+        [Required]
+        public int AnoValidade { get; set; }
+
+        [Required]
+        public int CodigoSeguranca { get; set; }
+
+        [Required]
+        public EStatusCartao Status { get; set; }
+
+        //[Required]
+        //[ForeignKey()]
+        //public int IdCiclista { get; set; }
+
+        //[Required]
+        //[ForeignKey]
+        //public IList<Emprestimo> Emprestimos { get; set; }
+
+        //[Required]
+        //[ForeignKey]
+        //public IList<Devolucao> Devolucoes { get; set; }
+    }
+}
