30bfe3bb79c6166fc2da81eca3ed03b1450a24e0
Matheus Varella <varellamat@gmail.com>
2023-02-28 13:22:14 -0300

    Implementado a classe emprestimo

---
+++ Aluguel/Models/Emprestimo.cs
@@ -0,0 +1,13 @@
+﻿namespace Aluguel.Models
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public float Valor { get; set; }
+        public DateTime DataHoraPagamento { get; set; }
+        public int CiclistaId { get; set; }
+        public int BicicletaId { get; set; }
+        public int TrancaId { get; set; }
+    }
+}
