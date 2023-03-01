35ea605cd475071895bc82b79b38a5ac9da77cbb
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-01 01:45:19 -0300

    refactor: aletrações necessárias para configurar tabela no banco de dados

--- Aluguel/Models/Emprestimo.cs
+++ Aluguel/Models/Emprestimo.cs
@@ -5,9 +5,12 @@
         public int Id { get; set; }
         public DateTime DataHora { get; set; }
         public float Valor { get; set; }
-        public DateTime DataHoraPagamento { get; set; }
+        public DateTime? DataHoraPagamento { get; set; }
         public int CiclistaId { get; set; }
         public int BicicletaId { get; set; }
         public int TrancaId { get; set; }
+        public virtual Ciclista Ciclista { get; set; }      
+        public virtual Bicicleta Bicicleta { get; set; }
+        public virtual Tranca Tranca { get; set; }        
     }
 }
