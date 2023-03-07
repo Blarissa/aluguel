240c23b375ae6f0757b696938d952675f2dcf7c0
Matheus Varella <varellamat@gmail.com>
2023-03-06 21:36:38 -0300

    feat: configurando tabela funcionario

---
+++ Aluguel/Data/Configuracoes/FuncionarioConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Aluguel.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Aluguel.Data.Configuracoes
+{
+    public class FuncionarioConfiguration : IEntityTypeConfiguration<Funcionario>
+    {
+        public void Configure(EntityTypeBuilder<Funcionario> builder)
+        {
+            builder.Property(f => f.Id).ValueGeneratedOnAdd();
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Matricula)
+                .IsRequired();
+
+            builder.Property(f => f.Nome)
+                .IsRequired();
+
+            builder.Property(f => f.Email)
+                .IsRequired();
+
+            builder.Property(f => f.Senha)
+                .IsRequired();
+
+            builder.Property(f => f.Cpf)
+                .IsRequired();
+
+            builder.Property<DateTime>(f => f.DataNascimento)
+                .IsRequired()
+                .HasColumnType("date");
+        
+            builder.Property(f => f.Funcao)
+                .IsRequired();
+        }
+    }
+}
