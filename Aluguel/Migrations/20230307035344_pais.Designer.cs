a5f631a2231760eb9553ec7ab8e975f6b4f11788
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 00:54:29 -0300

    feat: adicionado migration da tabela paises

--- Aluguel/Migrations/20230307035344_pais.Designer.cs
+++ Aluguel/Migrations/20230307035344_pais.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Aluguel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Aluguel.Migrations
 {
     [DbContext(typeof(AluguelContexto))]
-    partial class AluguelContextoModelSnapshot : ModelSnapshot
+    [Migration("20230307035344_pais")]
+    partial class pais
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Aluguel/Migrations/20230307035344_pais.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aluguel.Migrations
+{
+    public partial class pais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "paises",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    codigo = table.Column<string>(type: "varchar(2)", nullable: false),
+                    nome = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_paises", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "paises");
+        }
+    }
+}
