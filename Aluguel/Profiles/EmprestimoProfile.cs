d87254e7880f5b85485b14eb8784e413363a2f73
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 14:52:50 -0300

    feat: implementando EmprestimoProfile

---
+++ Aluguel/Profiles/EmprestimoProfile.cs
@@ -0,0 +1,14 @@
+﻿using Aluguel.Data.Dtos;
+using Aluguel.Models;
+using AutoMapper;
+
+namespace Aluguel.Profiles
+{
+    public class EmprestimoProfile : Profile
+    {
+        public EmprestimoProfile()
+        {
+            CreateMap<CreateEmprestimoDto, Emprestimo>();
+        }
+    }
+}
