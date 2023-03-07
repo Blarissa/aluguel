4b2ea127ef3c46b7d0da844f8b759d0034bd14a1
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 00:53:16 -0300

    feat: adicionado migration da tabela funcionario

---
+++ Aluguel/Migrations/20230307035156_funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aluguel.Migrations
+{
+    public partial class funcionario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "funcionarios",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    matricula = table.Column<string>(type: "text", nullable: false),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false),
+                    cpf = table.Column<string>(type: "text", nullable: false),
+                    data_nascimento = table.Column<DateTime>(type: "date", nullable: false),
+                    funcao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_funcionarios", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "funcionarios");
+        }
+    }
+}
