970d84a70a1fccdfe6fab9ece6132d380e961255
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 00:49:08 -0300

    feat: implementando dto do emprestimo

---
+++ Aluguel/Data/Dtos/Emprestimo/CreateEmprestimoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Aluguel.Data.Dtos;
+
+public class CreateEmprestimoDto 
+{
+    public Guid BicicletaId { get; set; }    
+    public Guid TrancaFimId { get; set; }
+    public Guid CobrancaId { get; set; }       
+    public DateTime DataHora { get; set; }
+    public DateTime DataHoraPagamento { get; set; }
+}
