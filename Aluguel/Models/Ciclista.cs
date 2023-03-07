39fb6902567c7acbe516b032e6a9a5d3090cbed4
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 18:05:28 -0300

    refactor: alterando algumas propriedade do ciclista para a migration

--- Aluguel/Models/Ciclista.cs
+++ Aluguel/Models/Ciclista.cs
@@ -7,19 +7,24 @@
         public DateTime DataNascimento { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string UrlFotoDocumento { get; set; }
-        public EStatusCiclista Status { get; set; }
-        public ENacionalidade Nacionalidade { get; set; }
+        public Uri UrlFotoDocumento { get; set; }
+        public int Status { get; set; }
+        public int Nacionalidade { get; set; }
         public DateTime DataHoraCadastro { get; set; }
         public DateTime DataHoraConfirmacao { get; set; }
+        
+        public string? Cpf { get; set; }
+        public Guid? PassaporteId { get; set; }
+
+        public virtual Passaporte Passaporte { get; set; }
         public virtual IList<CartaoDeCredito> Cartoes { get; set; }
         public virtual IList<Emprestimo> Emprestimos { get; set; }
-        public Guid PassaporteId { get; set; }
-        public string Cpf { get; set; }
+
 
         public Ciclista()
         {
             Emprestimos = new List<Emprestimo>();
+            Cartoes = new List<CartaoDeCredito>();
         }
     }
 }
