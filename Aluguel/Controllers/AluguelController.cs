cee4bc987dc1b460af1ef5536126e972b4be151c
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-07 00:48:39 -0300

    feat: iniciando implementação do AluguelController

---
+++ Aluguel/Controllers/AluguelController.cs
@@ -0,0 +1,34 @@
+﻿using Aluguel.Data;
+using Aluguel.Data.Dtos;
+using Aluguel.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aluguel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AluguelController : ControllerBase
+    {
+        private AluguelContexto contexto;
+        private IMapper mapper;
+
+        public AluguelController(AluguelContexto contexto, IMapper mapper)
+        {
+            this.contexto = contexto;
+            this.mapper = mapper;
+        }
+      
+        [HttpPost]
+        public IActionResult RealizaEmprestimo(Guid ciclistaId, Guid trancaId, 
+            [FromBody] CreateEmprestimoDto emprestimoDto)
+        {
+            var aluguel = mapper.Map<Emprestimo>(emprestimoDto);
+
+            contexto.Emprestimos.Add(aluguel);
+            contexto.SaveChanges();
+
+            return Ok(aluguel);
+        }
+    }
+}
