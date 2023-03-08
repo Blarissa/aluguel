1843b020cd1b7aaf97499777d498bbe02f5588dc
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-03-08 17:05:50 -0300

    feat: CartaoDeCreditoDto e Mapping profile

---
+++ Aluguel/Data/Dtos/Cartao/UpdateCartaoDeCreditoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Aluguel.Data.Dtos.Cartao
+{
+    public class UpdateCartaoDeCreditoDto
+    {
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public string Validade { get; set; }
+        public int CodigoSeguranca { get; set; }
+    }
+}
---
+++ Aluguel/Profiles/CartaoDeCreditoProfile.cs
@@ -0,0 +1,15 @@
+﻿using Aluguel.Data.Dtos.Cartao;
+using Aluguel.Models;
+using AutoMapper;
+
+namespace Aluguel.Profiles
+{
+    public class CartaoDeCreditoProfile : Profile
+    {
+        public CartaoDeCreditoProfile()
+        {
+            CreateMap<CartaoDeCredito,ReadCartaoDto>();
+            CreateMap<UpdateCartaoDeCreditoDto, CartaoDeCredito>();
+        }
+    }
+}
