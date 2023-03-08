2ae07d1f42aa47c411fbd7e055965cea009155bd
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-08 16:37:08 -0300

    refactor: altualizando classe UpdateFuncionario

--- Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
+++ Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
@@ -4,6 +4,7 @@ public class UpdateFuncionarioDto
 {
     public string Nome { get; set; }
     public string Senha { get; set; }
+    public string ConfirmaSenha { get; set; }
     public int Idade { get; set; }
     public int Funcao { get; set; }
 }
