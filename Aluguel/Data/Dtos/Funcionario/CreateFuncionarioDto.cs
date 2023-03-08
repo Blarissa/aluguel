235254b5665eca6972f70fd022e5049180c7156b
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-08 02:23:16 -0300

    refactor: adicionando propriedade para validacao dos dados

--- Aluguel/Data/Dtos/Funcionario/CreateFuncionarioDto.cs
+++ Aluguel/Data/Dtos/Funcionario/CreateFuncionarioDto.cs
@@ -8,6 +8,7 @@ public class CreateFuncionarioDto
     public string Nome { get; set; }
     public string Email { get; set; }
     public string Senha { get; set; }
+    public string ConfirmaSenha { get; set; }
     public string Cpf { get; set; }
     public int Idade { get; set; }
     public int Funcao { get; set; }
