4b377825ca2c9926b6c2be4549b177f9b36d53ad
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-10 13:09:47 -0300

    feat: adicionando DataAnnotations

--- Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
+++ Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
@@ -1,10 +1,23 @@
-﻿namespace Aluguel.Data.Dtos;
+﻿using Aluguel.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aluguel.Data.Dtos;
 
 public class UpdateFuncionarioDto
 {
-    public string Nome { get; set; }
+    [Required]
+    public string Nome { get; set; }        
+    
+    [Required]
     public string Senha { get; set; }
+    
+    [Required]
     public string ConfirmaSenha { get; set; }
+    
+    [Required]
     public int Idade { get; set; }
-    public int Funcao { get; set; }
+    
+    [Required]
+    [EnumDataType(typeof(EFuncao))]
+    public string Funcao { get; set; }
 }
