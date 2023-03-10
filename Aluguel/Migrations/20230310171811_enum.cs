ca18173d3527dda4e7653131b4b7ca379183c1de
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-10 14:22:33 -0300

    refactor: alterando migration para usar enum

---
+++ Aluguel/Migrations/20230310171811_enum.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aluguel.Migrations
+{
+    public partial class @enum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:e_funcao", "administrativo,reparador")
+                .Annotation("Npgsql:Enum:e_nacionalidade", "brasileiro,estrangeiro")
+                .Annotation("Npgsql:Enum:e_status_cartao", "ativo,desativado")
+                .Annotation("Npgsql:Enum:e_status_ciclista", "pendente,ativo,bloqueado");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "funcao",
+                table: "funcionarios",
+                type: "e_funcao",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:Enum:e_funcao", "administrativo,reparador")
+                .OldAnnotation("Npgsql:Enum:e_nacionalidade", "brasileiro,estrangeiro")
+                .OldAnnotation("Npgsql:Enum:e_status_cartao", "ativo,desativado")
+                .OldAnnotation("Npgsql:Enum:e_status_ciclista", "pendente,ativo,bloqueado");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "funcao",
+                table: "funcionarios",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "e_funcao");
+        }
+    }
+}
