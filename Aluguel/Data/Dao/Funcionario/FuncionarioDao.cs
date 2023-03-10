c8425dd96e2369266e0a7cc88d26e5acffc15dbd
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-10 15:25:29 -0300

    refactor: adicionando dao para funcionario

---
+++ Aluguel/Data/Dao/Funcionario/FuncionarioDao.cs
@@ -0,0 +1,38 @@
+﻿using Aluguel.Migrations;
+using Aluguel.Models;
+
+namespace Aluguel.Data.Dao;
+
+public class FuncionarioDao
+{
+    private AluguelContexto contexto;
+
+    public FuncionarioDao(AluguelContexto contexto)
+    {
+        this.contexto = contexto;
+    }
+
+    public void AdicionaFuncionario(Funcionario funcionario)
+    {
+        contexto.Funcionarios.Add(funcionario);
+        contexto.SaveChanges();
+    }
+
+    public void DeletaFuncionario(Funcionario funcionario)
+    {
+        contexto.Funcionarios.Remove(funcionario);
+        contexto.SaveChanges();
+    }
+
+    public Funcionario? RecuperaFuncionarioPorId(int idFuncionario)
+    {
+        return contexto.Funcionarios
+            .FirstOrDefault(f => f.Matricula == idFuncionario);
+    }
+
+    public IList<Funcionario> RecuperaTodosFuncionarios()
+    {
+        return contexto.Funcionarios.ToList();
+    }
+
+}
