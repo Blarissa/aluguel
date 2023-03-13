32eee7bc322ebf0a811bc56aaa9aa2c6dd7cca48
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-03-12 22:36:34 -0300

    feat: adicionados mocks de api externo e equipamento

---
+++ Aluguel/Mock/Equipamento/BicicletaController.cs
@@ -0,0 +1,26 @@
+﻿using Aluguel.Data.Dtos.Servicos.Equipamento;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aluguel.Mock.Equipamento
+{
+    [Route("mock/[controller]")]
+    [ApiController]
+    public class BicicletaController : ControllerBase
+    {
+
+        [HttpGet("{idBicicleta}")]
+        public IActionResult MockBicicletaPorId(Guid idBicicleta)
+        {
+            var retorno = new GetBicicletaPorIdDto() { 
+                Id = Guid.NewGuid(),
+                Ano = "2020",
+                Marca = "CALOI", 
+                Modelo = "BMX",
+                Numero = 456,
+                Status = Models.EStatusBicicleta.EM_USO 
+            };
+
+            return Ok(retorno);
+        }
+    }
+}
