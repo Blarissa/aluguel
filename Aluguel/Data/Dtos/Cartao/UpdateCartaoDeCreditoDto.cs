ee2ef2eb90aad829f554bdc91335d34e60cb86de
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-03-13 14:07:59 -0300

    feat: validacao com annotations

--- Aluguel/Data/Dtos/Cartao/UpdateCartaoDeCreditoDto.cs
+++ Aluguel/Data/Dtos/Cartao/UpdateCartaoDeCreditoDto.cs
@@ -1,10 +1,23 @@
-﻿namespace Aluguel.Data.Dtos.Cartao
+﻿using Aluguel.Validacao;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aluguel.Data.Dtos.Cartao
 {
     public class UpdateCartaoDeCreditoDto
     {
+        [Required]
+        [StringLength(60, ErrorMessage = "Nome é obrigatório", MinimumLength = 5)]
         public string Nome { get; set; }
+
+        [Required]
+        [CreditCard]
         public string Numero { get; set; }
+
+        [Required]
+        [DataFutura]
         public string Validade { get; set; }
+        [Required]
+        [Range(001, 999)]
         public int CodigoSeguranca { get; set; }
     }
 }
---
+++ Aluguel/Validacao/DataFuturaAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Aluguel.Validacao
+{
+    public class DataFuturaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value is string) {
+
+                string stringData = value as string ?? "";
+
+                Regex dataRegex = new Regex(@"(\d){4}-(\d){2}-(\d){2}");
+                if(!dataRegex.IsMatch(stringData)) return new ValidationResult("Formato inválido");
+
+                string[] valoresCampos = stringData.Split('-');
+
+                //converte para datetime os campos de ano, mes, dia
+                DateTime validadeEmDate = new DateTime(
+                int.Parse(valoresCampos[0]),
+                int.Parse(valoresCampos[1]),
+                int.Parse(valoresCampos[2])
+                );
+
+                //cartao vencido
+                if(validadeEmDate.CompareTo(DateTime.Today) < 0) return new ValidationResult("Cartão Vencido");
+
+                return ValidationResult.Success;
+
+            }
+            return new ValidationResult("Formato inválido");
+        }
+
+    }
+}
