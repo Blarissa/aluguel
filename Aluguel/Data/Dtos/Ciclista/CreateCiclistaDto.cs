83cb36014cc9d92c77bf31ce4a29e76ebdc1e7a2
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-14 17:05:41 -0300

    refactor: adicionando annotations

--- Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
+++ Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
@@ -1,13 +1,23 @@
-﻿namespace Aluguel.Data.Dtos.Ciclista
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Aluguel.Data.Dtos.Ciclista
 {
     public class CreateCiclistaDto
     {
         public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
+
         public string? Cpf { get; set; }
         public PassaporteDto? Passaporte { get; set; }
-        public int Nacionalidade { get; set; }
+        public string Nacionalidade { get; set; }
+        
+        [EmailAddress]
         public string Email { get; set; }
+
+        [DataType(DataType.ImageUrl)]
         public Uri UrlFotoDocumento { get; set; }
         public string Senha { get; set; }
     }
