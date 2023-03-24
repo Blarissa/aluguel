b748a0452e65bb445adedb0cc01d07fe8299b3a2
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-03-24 20:20:20 -0300

    feat: criando projeto de teste xUnit #7918

--- Aluguel/Program.cs
+++ Aluguel/Program.cs
@@ -1,86 +1,80 @@
 using Aluguel.Data;
 using Aluguel.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+    
+var builder = WebApplication.CreateBuilder(args);
 
-internal class Program
-{
-    private static void Main(string[] args)
+var connectionString = builder.Configuration.GetConnectionString("AluguelConnection");
+
+builder.Services.AddDbContext<AluguelContexto>(
+    options => options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson()
+    .ConfigureApiBehaviorOptions(options =>
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        var connectionString = builder.Configuration.GetConnectionString("AluguelConnection");
-
-        builder.Services.AddDbContext<AluguelContexto>(
-            options => options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
-
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-        builder.Services.AddHttpClient();
-
-        builder.Services.AddControllers()
-            .AddNewtonsoftJson()
-            .ConfigureApiBehaviorOptions(options =>
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var response = new
             {
-                options.InvalidModelStateResponseFactory = context =>
-                {
-                    var response = new
-                    {
-                        Erros = new List<Erro>()
-                    };
+                Erros = new List<Erro>()
+            };
 
-                    foreach (var (key, value) in context.ModelState)
-                        foreach (var item in value.Errors)
-                            response.Erros.Add(new Erro(key, item.ErrorMessage));
+            foreach (var (key, value) in context.ModelState)
+                foreach (var item in value.Errors)
+                    response.Erros.Add(new Erro(key, item.ErrorMessage));
 
-                    return new UnprocessableEntityObjectResult(response);
-                };
-            });
+            return new UnprocessableEntityObjectResult(response);
+        };
+    });
 
-        builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 
-        builder.Services.AddSwaggerGen(a =>
+builder.Services.AddSwaggerGen(a =>
+{
+    a.SwaggerDoc("v1",
+        new OpenApiInfo
         {
-            a.SwaggerDoc("v1",
-                new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Aluguel",
-                    Description = "Microsserviço Aluguel",
-                    License = new OpenApiLicense
-                    {
-                        Name = "Apache 2.0",
-                        Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
-                    }
-                });            
+            Version = "v1",
+            Title = "Aluguel",
+            Description = "Microsserviço Aluguel",
+            License = new OpenApiLicense
+            {
+                Name = "Apache 2.0",
+                Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+            }
+        });            
 
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
-            a.IncludeXmlComments(xmlPath);
-        });
+    a.IncludeXmlComments(xmlPath);
+});
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        //app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(options => 
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Aluguel V1"));
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => 
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Aluguel V1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();    
+
+public partial class Program { }
---
+++ TesteAluguel/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TesteAluguel
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
