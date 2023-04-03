bf9c414c5f19b4901d783406dbd7e1cb2152d499
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-02 23:01:43 -0300

    refactor: removendo DataAnnotations

--- Aluguel/Data/Dtos/Funcionario/ReadFuncionarioDto.cs
+++ Aluguel/Data/Dtos/Funcionario/ReadFuncionarioDto.cs
@@ -1,28 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Aluguel.Data.Dtos;
+﻿namespace Aluguel.Data.Dtos;
 
 public class ReadFuncionarioDto
-{
-    [Required]
-    public int Matricula { get; set; }
-
-    [Required]
-    public string Senha { get; set; }
-    
-    [Required]
-    [EmailAddress]
+{    
+    public int Matricula { get; set; }   
+    public string Senha { get; set; }    
     public string Email { get; set; }
-
-    [Required]    
-    public string Nome { get; set; }
-    
-    [Required]    
+    public string Nome { get; set; }    
     public int Idade { get; set; }
-
-    [Required]    
     public string Funcao { get; set; }
-    
-    [Required]
     public string Cpf { get; set; }    
 }
