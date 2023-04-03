1591f3df688e0f2fe6cf444200fafdba05ac1e2d
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-02 23:01:43 -0300

    refactor: removendo DataAnnotations

--- Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
+++ Aluguel/Data/Dtos/Funcionario/UpdateFuncionarioDto.cs
@@ -1,35 +1,11 @@
-﻿using Aluguel.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-
-namespace Aluguel.Data.Dtos;
+﻿namespace Aluguel.Data.Dtos;
 
 public class UpdateFuncionarioDto
 {
-    [Required]
-    [ModelBinder(Name = Erros.SenhaCod)]
-    [Compare("ConfirmaSenha", ErrorMessage = Erros.SenhaMsg)]
-    public string Senha { get; set; }
-    
+    public string Senha { get; set; }   
     public string ConfirmaSenha { get; set; }
-
-    [Required]
-    [ModelBinder(Name = Erros.EmailCod)]
-    [EmailAddress(ErrorMessage = Erros.EmailMsg)]
     public string Email { get; set; }
-
-    [Required]
-    [ModelBinder(Name = Erros.NomeCod)]
-    [MinLength(5, ErrorMessage = Erros.NomeMsg)]
     public string Nome { get; set; }
-
-    [Required]
-    [ModelBinder(Name = Erros.IdadeCod)]
-    [Range(18, 100, ErrorMessage = Erros.IdadeMsg)]
     public int Idade { get; set; }
-
-    [Required]
-    [ModelBinder(Name = Erros.FuncaoCod)]
-    [EnumDataType(typeof(EFuncao), ErrorMessage = Erros.FuncaoMsg)]
     public string Funcao { get; set; }
 }
