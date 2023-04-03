c61a4da3daca8e556e5509234497b61ef828e0a3
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-02 23:03:18 -0300

    refactor: iniciando implementação dos handlers do funcionário

---
+++ Aluguel/Handlers/Funcionarios/AdicionaFuncionarioHandler.cs
@@ -0,0 +1,23 @@
+﻿using Aluguel.Commands.Contracts;
+using Aluguel.Commands.Funcionarios;
+using Aluguel.Handlers.Contracts;
+using Aluguel.Models;
+using Aluguel.Repositorios.Contracts;
+
+namespace Aluguel.Handlers.Funcionarios
+{
+    public class AdicionaFuncionarioHandler : IHandler<AdicionaFuncionarioCommand>
+    {
+        private readonly IFuncionarioRepository _repository;
+
+        public AdicionaFuncionarioHandler(IFuncionarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(AdicionaFuncionarioCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
