5180c92667bc7247060c9ad09405a7bd64c2747c
Matheus Varella <varellamat@gmail.com>
2023-04-02 22:27:48 -0300

    feature : adicionado classe base para validação

---
+++ Aluguel/Validacao/BaseValidacao.cs
@@ -0,0 +1,16 @@
+﻿using Aluguel.Models;
+
+namespace Aluguel.Validacao;
+
+public abstract class BaseValidacao
+{
+    protected BaseValidacao() => Erros = new List<Erro>();
+
+    public bool Valida => Erros.Any() == false;
+    public List<Erro> Erros { get; private set; }
+
+    public void AdicionarErro(Erro erro)
+    {
+        Erros.Add(erro);
+    }
+}
