3bb5b9f2f17c43f948224edb6e7ebe3db011794d
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-03 04:33:54 -0300

    refactor: inserindo IHanderQuery

--- Aluguel/Handlers/Contracts/IHandler.cs
+++ Aluguel/Handlers/Contracts/IHandler.cs
@@ -1,7 +1,14 @@
 ﻿using Aluguel.Commands.Contracts;
+using Aluguel.Queries.Contracts;
+using System.Runtime.InteropServices;
 
 namespace Aluguel.Handlers.Contracts;
 
 public interface IHandler<T> where T : ICommand {
     ICommandResult Handle(T command);
+}
+
+public interface IHandlerQuery<T> where T : IQuery
+{
+    ICommandResult Handle([Optional]T query);
 }
