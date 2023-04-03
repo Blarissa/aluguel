5f3a5582423be59a936a0a4396aeb04704d31cb0
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-02 23:02:27 -0300

    refactor: iniciando implementação dos commands do funcionário

---
+++ Aluguel/Commands/Funcionarios/AdicionaFuncionarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using Aluguel.Commands.Contracts;
+using Aluguel.Data.Dtos;
+using Aluguel.Data.Dtos.Ciclista;
+using Aluguel.Models;
+
+namespace Aluguel.Commands.Funcionarios
+{
+    public class AdicionaFuncionarioCommand : ICommand
+    {
+        CreateFuncionarioDto funcionarioDto { get; set; }
+
+        public AdicionaFuncionarioCommand(CreateFuncionarioDto funcionarioDto)
+        {
+           this.funcionarioDto = funcionarioDto;
+        }             
+
+        public bool Validar()
+        {
+            return true;
+        }
+    }
+}
---
+++ Aluguel/Commands/Funcionarios/DeletaFuncionarioCommand.cs
@@ -0,0 +1,20 @@
+﻿using Aluguel.Commands.Contracts;
+using Aluguel.Data.Dtos;
+
+namespace Aluguel.Commands.Funcionarios
+{
+    public class DeletaFuncionarioCommand : ICommand
+    {
+        int Matricula { get; set; }
+
+        public DeletaFuncionarioCommand(int Matricula)
+        {
+            this.Matricula = Matricula;
+        }
+
+        public bool Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
