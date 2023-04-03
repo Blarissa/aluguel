1a0ac2b831964f9194d6a819fb5394509c863e73
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-03 04:37:14 -0300

    refactor: adicionando método de alterar funcionário

--- Aluguel/Repositorios/Contracts/IFuncionarioRepository.cs
+++ Aluguel/Repositorios/Contracts/IFuncionarioRepository.cs
@@ -4,7 +4,8 @@ namespace Aluguel.Repositorios.Contracts;
 
 public interface IFuncionarioRepository 
 {
-    void Adicionar(Funcionario funcionario);    
+    void Adicionar(Funcionario funcionario);
+    void Alterar(Funcionario funcionario);
     void Deletar(Funcionario funcionario);
     IList<Funcionario> RecuperarTodos();
     Funcionario RecuperarPorMatricula(int matricula);
