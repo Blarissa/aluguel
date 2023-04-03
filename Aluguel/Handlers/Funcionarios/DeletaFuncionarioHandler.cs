a206a7855be2c511045ff065d16e0a5881a8eeb2
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-02 23:03:18 -0300

    refactor: iniciando implementação dos handlers do funcionário

---
+++ Aluguel/Handlers/Funcionarios/DeletaFuncionarioHandler.cs
@@ -0,0 +1,22 @@
+﻿using Aluguel.Commands.Contracts;
+using Aluguel.Commands.Funcionarios;
+using Aluguel.Handlers.Contracts;
+using Aluguel.Repositorios.Contracts;
+
+namespace Aluguel.Handlers.Funcionarios
+{
+    public class DeletaFuncionarioHandler : IHandler<DeletaFuncionarioCommand>
+    {
+        private readonly IFuncionarioRepository _repository;
+
+        public DeletaFuncionarioHandler(IFuncionarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(DeletaFuncionarioCommand command)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
