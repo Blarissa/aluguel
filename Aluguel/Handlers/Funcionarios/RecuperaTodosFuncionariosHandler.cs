a6f5c0ba53f15d963b145bc30cfbcd9616060c2b
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-03 04:35:12 -0300

    refactor: implementando queries e handlers de recuperação dos funcionários

---
+++ Aluguel/Handlers/Funcionarios/RecuperaTodosFuncionariosHandler.cs
@@ -0,0 +1,31 @@
+﻿using Aluguel.Commands;
+using Aluguel.Commands.Contracts;
+using Aluguel.Data.Dtos;
+using Aluguel.Handlers.Contracts;
+using Aluguel.Models;
+using Aluguel.Queries.Funcionarios;
+using Aluguel.Repositorios.Contracts;
+using AutoMapper;
+using System.Runtime.InteropServices;
+
+namespace Aluguel.Handlers.Funcionarios
+{
+    public class RecuperaTodosFuncionariosHandler : IHandlerQuery<RecuperaTodosFuncionariosQuery>
+    {
+        private readonly IFuncionarioRepository _repository;
+        private readonly IMapper _mapper;
+
+        public RecuperaTodosFuncionariosHandler(IFuncionarioRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public ICommandResult Handle([Optional] RecuperaTodosFuncionariosQuery query)
+        {
+            var funcionarios = _mapper.Map<List<ReadFuncionarioDto>>(_repository.RecuperarTodos());
+
+            return new GenericCommandResult(funcionarios);
+        }
+    }
+}
---
+++ Aluguel/Queries/Funcionarios/RecuperaFuncionarioPorMatriculaQuery.cs
@@ -0,0 +1,17 @@
+﻿using Aluguel.Data.Dtos;
+using Aluguel.Models;
+using Aluguel.Queries.Contracts;
+using System.Linq.Expressions;
+
+namespace Aluguel.Queries.Funcionarios
+{
+    public class RecuperaFuncionarioPorMatriculaQuery : IQuery
+    {
+        public int Matricula { get; set; }        
+        
+        public RecuperaFuncionarioPorMatriculaQuery(int matricula)
+        {
+            Matricula = matricula;
+        }
+    }
+}
