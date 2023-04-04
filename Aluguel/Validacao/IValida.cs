103b762ea0c49d5c8697e90437fe5699dc249244
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-04 00:24:22 -0300

    refactor: iniciando implementação de validações

---
+++ Aluguel/Validacao/IValida.cs
@@ -0,0 +1,21 @@
+﻿using Aluguel.Models;
+
+namespace Aluguel.Validacao
+{
+    public interface IValida
+    {
+        bool CPF(string cpf);
+        bool Data(string data);
+        bool DataHora(string dataHora);
+        bool Email(string email);
+        bool Funcao(string funcao);
+        bool Idade(string idade);
+        bool Nacionalidade(string nacionalidade);
+        bool Matricula(int matricula);
+        bool Id(Guid id);
+        bool Nome(string nome);        
+        bool Passaporte(Passaporte passaporte);
+        bool Senha(string senha, string confirmaSenha);              
+        bool UrlFotoDocumento(string foto);
+    }
+}
---
+++ Aluguel/Validacao/Valida.cs
@@ -0,0 +1,71 @@
+﻿using Aluguel.Migrations;
+using Aluguel.Models;
+
+namespace Aluguel.Validacao
+{
+    public class Valida : IValida
+    {
+        public bool CPF(string cpf)
+        {
+            return ValidaFormato.CPFFormato(cpf) && 
+                   ValidaRegras.CPFRegras(cpf);
+        }
+
+        public bool Data(string data)
+        {
+            return ValidaFormato.DataFormato(data);
+        }
+
+        public bool DataHora(string dataHora)
+        {
+            return ValidaFormato.DataHoraFormato(dataHora);
+        }
+
+        public bool Email(string email)
+        {
+            return ValidaFormato.EmailFormato(email);
+        }
+
+        public bool Funcao(string funcao)
+        {
+            return ValidaFormato.FuncaoFormato(funcao);
+        }
+
+        public bool Idade(string idade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Nacionalidade(string nacionalidade)
+        {
+            return ValidaFormato.NacionalideFormato(nacionalidade);
+        }
+        
+        public bool Nome(string nome)
+        {
+            return ValidaFormato.NomeFormato(nome);
+        }
+        
+        public bool Passaporte(Passaporte passaporte)
+        {
+            return Pais(passaporte.Pais);
+        }
+
+        
+        public bool Senha(string senha, string confirmaSenha)
+        {
+            return senha.Equals(confirmaSenha);
+        }                
+
+        //falta fazer
+        public bool UrlFotoDocumento(string foto)
+        {
+            return ValidaFormato.FotoFormato(foto);
+        }
+        
+        private bool Pais(Pais pais)
+        {
+            return ValidaFormato.PaisFormato(pais.Codigo);
+        }
+    }
+}
