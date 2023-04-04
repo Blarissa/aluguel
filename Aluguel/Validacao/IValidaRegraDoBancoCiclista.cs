074841f94d4e41e8e315f82cbc0d87f208c909d5
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-04 04:11:36 -0300

    refactor: implemantando validações usando dados do banco

---
+++ Aluguel/Validacao/IValidaRegraDoBancoCiclista.cs
@@ -0,0 +1,9 @@
+﻿namespace Aluguel.Validacao
+{
+    public interface IValidaRegraDoBancoCiclista
+    {
+        bool CPFCiclista(string cpf);        
+        bool IdCiclista(Guid idCiclista);
+        bool Passaporte(string codigo);
+    }
+}
---
+++ Aluguel/Validacao/ValidaRegraDoBancoCiclista.cs
@@ -0,0 +1,44 @@
+﻿using Aluguel.Data.Dao;
+using Aluguel.Models;
+using Aluguel.Repositorios.Contracts;
+
+namespace Aluguel.Validacao
+{
+    public class ValidaRegraDoBancoCiclista : IValidaRegraDoBancoCiclista
+    {
+        ICiclistaRepository _ciclistaRepository;
+        IPaisRepository _paisRepository;
+
+        public ValidaRegraDoBancoCiclista(ICiclistaRepository ciclistaRepository)
+        {
+            _ciclistaRepository = ciclistaRepository;
+        }        
+
+        public ValidaRegraDoBancoCiclista(IPaisRepository paisRepository)
+        {
+            _paisRepository = paisRepository;
+        }
+
+        //se existe algum ciclista com o cpf passado
+        public bool CPFCiclista(string cpf)
+        {
+            //return _ciclistaRepository.RecuperarTodos().Any(c => c.Cpf == cpf);
+            throw new NotImplementedException();
+        }
+
+        //se existe algum ciclista com o id passado
+        public bool IdCiclista(Guid idCiclista)
+        {
+            //return _ciclistaRepository.RecuperarTodos().Any(c => c.id == idCiclista);
+            throw new NotImplementedException();
+        }               
+
+        //se o código do pais existe
+        public bool Passaporte(string codigo)
+        {
+            return _paisRepository
+                .RecuperarTodos()
+                .Any(p => p.Codigo == codigo);
+        }
+    }
+}
