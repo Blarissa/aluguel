b4c12a6f156eec935073a4cfdf850c58d2b26f95
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-05 09:27:46 -0300

    style: removendo espaços em branco

--- Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
+++ Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
@@ -11,18 +11,10 @@ namespace Aluguel.Data.Dtos.Ciclista
     {        
         public string Nome { get; set; }        
         public DateTime DataNascimento { get; set; }
-
         public string? Cpf { get; set; }
-
         public PassaporteDto? Passaporte { get; set; }
-
-        
-        public string Nacionalidade { get; set; }
-        
-        
-        public string Email { get; set; }
-
-        
+        public string Nacionalidade { get; set; }        
+        public string Email { get; set;}
         public Uri UrlFotoDocumento { get; set; }          
         public string Senha { get; set; }
     }
