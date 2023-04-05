3e05deac1b5a59338c249daefb3acf7f0b386634
Larissa dos Santos Brasil <larissa.brasil@aluno.residencia.ed.dev.br>
2023-04-05 15:42:46 -0300

    refactor: iniciando implementações relacionadad ao aluguel

---
+++ Aluguel/Repositorios/Contracts/AluguelRepository.cs
@@ -0,0 +1,21 @@
+﻿using Aluguel.Data;
+using Aluguel.Models.Entidades;
+
+namespace Aluguel.Repositorios.Contracts
+{
+    public class AluguelRepository : IAluguelRepository
+    {
+        private AluguelContexto contexto;
+
+        public AluguelRepository(AluguelContexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public void RealizaAluguel(Emprestimo emprestimo)
+        {
+            contexto.Emprestimos.Add(emprestimo);
+            contexto.SaveChanges();
+        }
+    }
+}
--- Aluguel/Repositorios/Contracts/IAluguelRepository.cs
+++ Aluguel/Repositorios/Contracts/IAluguelRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace Aluguel.Repositorios.Contracts; 
+﻿using Aluguel.Models.Entidades;
+
+namespace Aluguel.Repositorios.Contracts; 
 
 public interface IAluguelRepository 
 {
-    
+    void RealizaAluguel(Emprestimo emprestimo);
 }
