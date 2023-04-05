0bd311047a8e07d728786df9bdd7b3eef4a1047a
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-04-05 15:13:59 -0300

    refactor: adicao de injecao de dependencias dos demais microsservicos usando interfaces

--- Aluguel/Servicos/Equipamento/EquipamentoApi.cs
+++ Aluguel/Servicos/Equipamento/EquipamentoApi.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 namespace Aluguel.Servicos.Bicicleta
 {
-    public class EquipamentoApi
+    public class EquipamentoApi : IEquipamentoService
     {
         private readonly HttpClient client;
         private readonly string baseUriServico;
@@ -23,25 +23,12 @@ namespace Aluguel.Servicos.Bicicleta
         {
             var resposta = await client.GetAsync(baseUriServico+$"/bicicleta/{idBicicleta}");
 
-
-
-            //GetBicicletaPorIdDto? valorTraduzido = JsonConvert.DeserializeObject<GetBicicletaPorIdDto>(await resposta.Content.ReadAsStringAsync());
-
-
-            //if(valorTraduzido == null)
-            //    return new GetBicicletaPorIdDto();
-
             return resposta;
         }
 
         public async Task<HttpResponseMessage> BuscarTrancaPorId(Guid idTranca)
         {
             var resposta = await client.GetAsync(baseUriServico+$"/tranca/{idTranca}");
-
-            //GetTrancaPorIdDto? valorTraduzido = JsonConvert.DeserializeObject<GetTrancaPorIdDto>(await resposta.Content.ReadAsStringAsync());
-
-            //if(valorTraduzido == null)
-            //    return new GetTrancaPorIdDto();
 
             return resposta;
         }
---
+++ Aluguel/Servicos/IExternoService.cs
@@ -0,0 +1,13 @@
+﻿using Aluguel.Data.Dtos.Servicos.Externo;
+
+namespace Aluguel.Servicos
+{
+    public interface IExternoService
+    {
+        public Task<HttpResponseMessage> EnviarCobrancaParaFila(PostFilaCobrancaDto cobrancaDto);
+        public Task<HttpResponseMessage> EnviarCobranca(PostCobrancaDto cobrancaDto);
+        public Task<HttpResponseMessage> EnviarEmail(PostEnviarEmailDto emailDto);
+        public Task<HttpResponseMessage> BuscarCobranca(Guid idCobranca);
+        public Task<HttpResponseMessage> ValidacaoCartao(PostValidaCartaoDto validaCartaoDto);
+    }
+}
