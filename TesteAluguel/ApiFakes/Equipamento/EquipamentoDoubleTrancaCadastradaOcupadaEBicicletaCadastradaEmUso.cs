5f8e95a90bc07153c10cf84776f4ce8fd8f543f7
João Eduardo Vasconcellos <joao.eduardovsilva@hotmail.com>
2023-04-05 18:47:31 -0300

    test: cobertura caso de uso 4

---
+++ TesteAluguel/ApiFakes/Equipamento/EquipamentoDoubleTrancaCadastradaOcupadaEBicicletaCadastradaEmUso.cs
@@ -0,0 +1,61 @@
+﻿using Aluguel.Data.Dtos.Servicos.Equipamento;
+using Aluguel.Models;
+using Aluguel.Models.Entidades;
+using Aluguel.Servicos;
+using Aluguel.Servicos.Bicicleta;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAluguel.ApiFake
+{
+    internal class EquipamentoDoubleTrancaCadastradaOcupadaEBicicletaCadastradaEmUso : IEquipamentoService
+    {
+        public EquipamentoDoubleTrancaCadastradaOcupadaEBicicletaCadastradaEmUso()
+        {
+        }
+
+        public Task<HttpResponseMessage> BuscarBicicletaPorId(Guid idBicicleta)
+        {
+                var body = new GetBicicletaPorIdDto(){
+                    Id = idBicicleta,
+                    Marca=  "Caloi",
+                    Modelo = "BMX",
+                    Ano= "2010",
+                    Numero = 10,
+                    Status = EStatusBicicleta.EM_USO
+                };
+
+                var retorno = new HttpResponseMessage();
+
+                retorno.Content = new StringContent(JsonConvert.SerializeObject(body));
+                retorno.StatusCode = System.Net.HttpStatusCode.OK;
+
+                return Task.FromResult(retorno);
+        }
+
+        public Task<HttpResponseMessage> BuscarTrancaPorId(Guid idTranca)
+        {
+            var body = new GetTrancaPorIdDto(){
+                Id = idTranca,
+                Numero = 10,
+                Localizacao = "Endereco da tranca",
+                AnoDeFabricacao = "2010",
+                Modelo = "Chaveboa",
+                Status = EStatusTranca.OCUPADA,
+            };
+
+            var retorno = new HttpResponseMessage();
+            retorno.Content = new StringContent(JsonConvert.SerializeObject(body));
+            retorno.StatusCode = System.Net.HttpStatusCode.OK;
+
+            return Task.FromResult(retorno);
+        }
+    }
+}
