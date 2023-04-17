c10d571c1c3090b3495f2bda7d6f81dbbf8c5e44
Larissa Brasil <larissabrasil009@gmail.com>
2023-04-17 00:14:50 -0300

    refactor: adicionando endpoints

--- Aluguel/Servicos/IEquipamentoService.cs
+++ Aluguel/Servicos/IEquipamentoService.cs
@@ -1,4 +1,5 @@
 ﻿using Aluguel.Data.Dtos.Servicos.Externo;
+using Aluguel.Models;
 
 namespace Aluguel.Servicos
 {
@@ -7,5 +8,8 @@ namespace Aluguel.Servicos
         public Task<HttpResponseMessage> BuscarBicicletaPorId(Guid idBicicleta);
         public Task<HttpResponseMessage> BuscarBicicletaPorTranca(Guid idTranca);
         public Task<HttpResponseMessage> BuscarTrancaPorId(Guid idTranca);
+        public Task<HttpResponseMessage> AlterarStatusBicicleta(Guid idBicicleta, EStatusBicicleta acao);
+        public Task<HttpResponseMessage> DestrancarTranca(Guid idTranca);
+        public Task<HttpResponseMessage> BuscarTotens();
     }
 }
