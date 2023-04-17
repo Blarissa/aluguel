1cafdfa4e7aebf25d230fd06e9f4e32fb80e3bf0
Larissa Brasil <larissabrasil009@gmail.com>
2023-04-17 00:21:37 -0300

    refactor: alterações necessárias para uso do handler

---
+++ Aluguel/Data/Dtos/Emprestimo/CreateDadosDeRetirada.cs
@@ -0,0 +1,23 @@
+﻿namespace Aluguel.Data.Dtos.Emprestimo
+{
+    public class CreateDadosDeRetirada
+    {
+        public DateTime DataHoraRetirada { get; set; }
+        public Guid IdTranca { get; set; }
+        public Guid IdBicicleta { get; set; }
+        public Guid IdCartao { get; set; }
+        public Guid IdCiclista { get; set; }
+        
+        public CreateDadosDeRetirada(DateTime dataHoraRetirada, 
+            Guid idTranca, Guid idBicicleta, Guid idCartao, 
+            Guid idCiclista)
+        {
+            DataHoraRetirada = dataHoraRetirada;
+            IdTranca = idTranca;
+            IdBicicleta = idBicicleta;
+            IdCartao = idCartao;
+            IdCiclista = idCiclista;
+        }
+
+    }
+}
---
+++ Aluguel/Data/Dtos/Emprestimo/ReadDadosRetiradaDto.cs
@@ -0,0 +1,49 @@
+﻿using Aluguel.Data.Dtos.Ciclista;
+using Aluguel.Data.Dtos.Servicos.Equipamento;
+using Aluguel.Data.Dtos.Servicos.Externo;
+using Aluguel.Models.Entidades;
+
+namespace Aluguel.Data.Dtos.Emprestimo
+{
+    public class ReadDadosRetiradaDto
+    {
+        public ReadBicicletaDto Bicicleta { get; set; }
+        public ReadTrancaDto Tranca { get; set; }
+        public ReadTotemDto Totem { get; set; }
+        public ReadCiclistaDto Ciclista { get; set; }
+        public ReadCobrancaDto Cobranca { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime? HoraFim { get; set; }                
+               
+        public ReadDadosRetiradaDto(ReadBicicletaDto bicicleta, 
+            ReadTrancaDto tranca, ReadTotemDto totem, 
+            ReadCiclistaDto ciclista, ReadCobrancaDto cobranca, 
+            DateTime horaInicio, DateTime? horaFim)
+        {
+            Bicicleta = bicicleta;
+            Tranca = tranca;
+            Totem = totem;
+            Ciclista = ciclista;
+            Cobranca = cobranca;
+            HoraInicio = horaInicio;
+            HoraFim = horaFim;
+        }
+
+        public override string ToString()
+        {            
+            return $"Dados de Retirada da bicicleta\n" +
+                   $"Horário de retirada: {HoraInicio: t}\n" +
+                   $"Horário previsto de devolução: {HoraFim: t}\n" +
+                   $"Dados da cobrança realizada\n" +
+                   $"{Cobranca}" +
+                   $"Dados do Ciclista que realizou a retirada\n " +
+                   $"{Ciclista}" +
+                   $"Dados da bicicleta retirada\n " +
+                   $"{Bicicleta}\n" +
+                   $"Tranca da Bicicleta\n" +
+                   $"{Tranca}" +                   
+                   $"Totem de bicicletas\n" +
+                   $"{Totem}\n" ;
+        }
+    }
+}
