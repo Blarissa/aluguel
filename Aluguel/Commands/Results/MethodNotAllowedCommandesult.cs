41e3ef596e830cc6cc56849aaae9d704ae787eba
Blarissa <larissabrasil009@gmail.com>
2023-04-23 17:36:55 -0300

    feat: adicionando CommandResult para status 201 e 405

---
+++ Aluguel/Commands/Results/MethodNotAllowedCommandesult.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Aluguel.Commands.Results
+{
+    public class MethodNotAllowedCommandesult : GenericCommandResult
+    {
+        public MethodNotAllowedCommandesult(object data) : base(data)
+        {
+            Status = HttpStatusCode.MethodNotAllowed;
+        }
+    }
+}
