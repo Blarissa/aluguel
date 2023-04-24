19ee6eca492ba9ddb4e094e59f8b182dbd2dea5d
Blarissa <larissabrasil009@gmail.com>
2023-04-24 20:48:35 -0300

    fix: resolvendo erro de adição de ciclista

--- Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
+++ Aluguel/Data/Dtos/Ciclista/CreateCiclistaDto.cs
@@ -13,24 +13,8 @@ namespace Aluguel.Data.Dtos.Ciclista
         public string Nome { get; set; }        
         public DateTime DataNascimento { get; set; }
         public string Nacionalidade { get; set; }        
-        public string? Cpf { 
-            get 
-            {
-                if(Nacionalidade.Equals("ESTRANGEIRO"))
-                    return null;
-
-                return Cpf;
-            }
-            set { } }
-        public CreatePassaporteDto? Passaporte { 
-            get 
-            {
-                if (Nacionalidade.Equals("BRASILEIRO"))
-                    return null;
-
-                return Passaporte;
-            }
-            set { } }
+        public string? Cpf { get; set; }
+        public CreatePassaporteDto? Passaporte { get; set; }
         public string Email { get; set; }  
         public Uri UrlFotoDocumento { get; set; }          
         public string Senha { get; set; }
