9530c3dd6c65a06136538d0e49c1741f2f40c30c
Blarissa <larissabrasil009@gmail.com>
2023-05-08 09:40:37 -0300

    test: iniciando testes para UC03

---
+++ TesteAluguel/AluguelControllerTestes/UC03/RealizarAluguelTeste.cs
@@ -0,0 +1,68 @@
+﻿using Aluguel.Data.Dtos.Cartao;
+using Aluguel.Data.Dtos.Ciclista;
+using Aluguel.Data.Dtos.Emprestimo;
+using Aluguel.Models;
+using Aluguel.Models.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TesteAluguel.AluguelControllerTestes.UC03
+{
+    internal class RealizarAluguelTeste : RealizarAluguelTesteBase
+    {
+        public RealizarAluguelTeste(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact(DisplayName = "CT01 - StatusCode")]
+        public void VerificaSeRealizarAluguelCorretoRetornaStatusPretendido()
+        {
+            var ciclista = AdicionaCiclista();
+            var tranca = AdicionaTranca();
+
+            var emprestimoDto = new CreateEmprestimoDto() { 
+                Ciclista = ciclista.Id, 
+                TrancaInicio = tranca.Id
+            };
+        }
+
+        private Tranca AdicionaTranca()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Ciclista AdicionaCiclista()
+        {
+            var ciclista = new CreateCiclistaDto()
+            {
+                Nome = "Renato Lucca Noah Aparício",
+                DataNascimento = DateTime.Parse("19/03/1953"),
+                Cpf = "56230950898",
+                Passaporte = null,
+                Nacionalidade = ENacionalidade.BRASILEIRO,
+                Email = "renato-aparicio70@mega.com.br",
+                UrlFotoDocumento = new Uri("https://www.SomeValidURI.co"),
+                Senha = "Ft3cY5pEzK",
+                ConfirmaSenha = "Ft3cY5pEzK"
+            };
+
+            var cartao = new CreateMeioDePagamentoDto()
+            {
+                Nome = "Renato Lucca Noah Aparício",
+                Numero = "4929685404235515",
+                MesValidade = 8,
+                AnoValidade = 24,
+                CodigoSeguranca = 643
+            }; 
+
+            return _ciclistaRepository
+                .AdicionarCiclistaComCartao();
+        }
+    }
+}
---
+++ TesteAluguel/AluguelControllerTestes/UC03/RealizarAluguelTesteBase.cs
@@ -0,0 +1,81 @@
+﻿using Aluguel.Commands.Alugueis;
+using Aluguel.Commands.Ciclistas;
+using Aluguel.Commands.Contracts;
+using Aluguel.Commands.Devolucoes;
+using Aluguel.Controllers;
+using Aluguel.Data;
+using Aluguel.Data.Dtos.Ciclista;
+using Aluguel.Data.Dtos.Emprestimo;
+using Aluguel.Handlers.Alugueis;
+using Aluguel.Handlers.Ciclistas;
+using Aluguel.Profiles;
+using Aluguel.Repositorios;
+using Aluguel.Repositorios.Contracts;
+using Aluguel.Servicos;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+
+namespace TesteAluguel.AluguelControllerTestes;
+
+public class RealizarAluguelTesteBase
+{
+    internal readonly WebApplicationFactory<Program> application;
+    internal readonly HttpClient client;
+    internal readonly IMapper _mapper;
+    internal readonly ITestOutputHelper output;
+    internal readonly string BaseUri = "https://localhost:7054";
+    internal readonly DbContextOptions _options;
+    internal readonly AluguelContexto _context;
+    internal readonly IAluguelRepository _aluguelRepository;
+    internal readonly ICiclistaRepository _ciclistaRepository;
+    internal readonly IPaisRepository _paisRepository;
+
+    internal readonly IEquipamentoService _equipamento;
+    internal readonly IExternoService _externo; 
+
+    public RealizarAluguelTesteBase(ITestOutputHelper output)
+    {
+        application = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                // Configure test services
+            });
+
+        client = application.CreateClient();
+
+        this.output = output;
+
+       // configurando inMemoryDatabase
+        _options = new DbContextOptionsBuilder<AluguelContexto>()
+            .UseInMemoryDatabase("Aluguel")
+            .Options;
+
+        _context = new AluguelContexto(_options);
+
+        _aluguelRepository = new AluguelRepository(_context);
+        _ciclistaRepository = new CiclistaRepository(_context);
+        _paisRepository = new PaisRepository(_context);        
+
+        //configurando IMapper
+        if (_mapper == null)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+                mc.AddProfile(new CiclistaProfile()));
+
+            _mapper = mappingConfig.CreateMapper();
+        }
+    }
+
+    //retona resultado do aluguel
+    public ICommandResult ResponseAtual(CreateEmprestimoDto emprestimoDto)
+    {
+        var handler = new RealizaAluguelHandler(_aluguelRepository, _mapper, 
+            _equipamento, _externo, _ciclistaRepository, _paisRepository);
+
+        var command = new RealizarAluguelCommand(emprestimoDto);
+
+        return handler.Handle(command).Result;
+    }
+}
